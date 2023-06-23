0c9b4a726476dcb83bee533cc64d8aa49f4ad1c5
Murshid <muhsink00000@gmail.com>
2023-06-23 20:50:50 +0530

    joining form

---
+++ controllers/main.py
@@ -0,0 +1,48 @@
+from odoo import http
+from odoo.http import request
+
+
+class WebsiteForm(http.Controller):
+    @http.route(['/employee_form'], type='http', auth="user", website=True)
+    def appointment(self):
+        partners = request.env['employee.module.form'].sudo().search([])
+        values = {}
+        values.update({
+            'partners': partners
+        })
+        return request.render("logic_employee_form.online_appointment_form", values)
+
+    @http.route(['/employee_form/submit'], type='http', auth="public", website=True, csrf=False)
+    def employee_form_submit(self, **kw):
+        print('hi')
+        request.env['employee.module.form'].sudo().create({
+            'employee_name': kw.get('employee_name'),
+            'phone_number': kw.get('phone_number'),
+            'designation': kw.get('designation'),
+            'date_of_joining': kw.get('joining'),
+            'mail_id': kw.get('mail_id'),
+            'address': kw.get('address'),
+            'date_of_birth': kw.get('birth'),
+            'marital_stats': kw.get('marital_stats'),
+            'spouse_name': kw.get('spouse_name'),
+            'name_of_children': kw.get('name_of_children'),
+            'pan_card_number': kw.get('pan_number'),
+            'name_as_per_pan': kw.get('pan_name'),
+            'pf_uan_number': kw.get('pf_number'),
+            'esi_ip_number': kw.get('esi_number'),
+            'blood_group': kw.get('blood_group'),
+            'name_as_per_aadhar': kw.get('name_as_per_aadhar'),
+            'aadhar_card_number': kw.get('aadhar_number'),
+            'bank_name': kw.get('bank_name'),
+            'bank_acc_number': kw.get('account_number'),
+            'branch_bank': kw.get('branch'),
+            'ifsc_code': kw.get('ifsc'),
+            'micr_code': kw.get('micr'),
+            'name_as_per_bank': kw.get('name_as_per_bank'),
+
+
+
+            # 'sale_order_id': kw.get('sale_order')
+        })
+        return request.render("logic_employee_form.logic_employee_form_success", {})
+
