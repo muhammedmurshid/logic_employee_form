0197b2f46513f7f46662eac02003a85bf23d744a
muhammedmurshid <muhsink00000@gmail.com>
2024-11-15 14:59:34 +0530

    add new group digital

--- models/link_employee.py
+++ models/link_employee.py
@@ -160,27 +160,3 @@ class LinkEmployeeJoiningForm(models.Model):
 
         else:
             raise UserError("Please Select Related Employee")
-
-            # if self.related_employee_id.photo:
-            #     self.photo = self.related_employee_id.photo,
-
-                # self.esi_ip_number = self.related_employee_id.esi_ip_number,
-            # self.blood_group = self.related_employee_id.blood_group,
-
-            # self.spouse_birthdate = self.related_employee_id.spouse_dob,
-            # self.spouse_complete_name = self.related_employee_id.spouse_name,
-            # self.children_name = self.related_employee_id.name_of_children,
-            # # 'fam_ids': abc,
-            # self.upload_cv = self.related_employee_id.upload_cv,
-            # self.aadhar_photo = self.related_employee_id.aadhar_photo,
-            # self.pan_photo = self.related_employee_id.pan_photo,
-            # self.bank_passbook = self.related_employee_id.bank_passbook,
-            # self.photo = self.related_employee_id.photo,
-            # self.private_email = self.related_employee_id.mail_id,
-            # self.work_phone = self.related_employee_id.office_phone
-
-        print('add_related_employee')
-
-
-
-
